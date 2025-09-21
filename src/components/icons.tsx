4d93ff187bd8bd79d1dc77abed768203ce40f865
Vinicius Watanabe Lima <v.watanabelima@gmail.com>
2025-09-21 16:07:00 +0000

    você nao entendeu, quero exatamente a mesma imagem que mandei para o ico

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,24 +1,11 @@
-import type { SVGProps } from 'react';
+import * as React from 'react';
 
 export const Icons = {
-  logo: (props: SVGProps<SVGSVGElement>) => (
-    <svg
+  logo: (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
+    <img
       {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 100 100"
-      width="100"
-      height="100"
-    >
-      <defs>
-        <linearGradient id="logo-gradient" x1="0%" y1="0%" x2="100%" y2="100%">
-          <stop offset="0%" style={{ stopColor: 'hsl(var(--primary))', stopOpacity: 1 }} />
-          <stop offset="100%" style={{ stopColor: 'hsl(var(--accent))', stopOpacity: 1 }} />
-        </linearGradient>
-      </defs>
-      <path
-        d="M 10 90 L 30 20 L 50 70 L 70 20 L 90 90 L 75 90 L 60 45 L 40 45 L 25 90 Z"
-        fill="url(#logo-gradient)"
-      />
-    </svg>
+      src="https://i.imgur.com/ipR9G0I.png"
+      alt="TurboConteúdo Logo"
+    />
   ),
 };
